a89e5aac308673cd19ddffec67db82ab106c954d
Tezos Chad <h1sp1d3r@gmail.com>
2024-06-25 21:39:29 -0400

    Update background.js
    optimize

--- js/background.js
+++ js/background.js
@@ -22,7 +22,21 @@
         this.y += this.velocity.y;
     };
 
-    Particle.prototype.h = function() {
+    Particle.prototype.drawConnection = function(otherParticle) {
+        var distance = Math.sqrt(Math.pow(this.x - otherParticle.x, 2) + Math.pow(this.y - otherParticle.y, 2));
+        if (distance > 120) {
+            return;
+        }
+        this.g.beginPath();
+        this.g.strokeStyle = this.particleColor;
+        this.g.globalAlpha = (120 - distance) / 120;
+        this.g.lineWidth = 0.7;
+        this.g.moveTo(this.x, this.y);
+        this.g.lineTo(otherParticle.x, otherParticle.y);
+        this.g.stroke();
+    };
+
+    Particle.prototype.draw = function() {
         this.g.beginPath();
         this.g.fillStyle = this.particleColor;
         this.g.globalAlpha = 0.7;
@@ -31,145 +45,91 @@
     };
 
     function ParticleNetwork(element, options) {
-        this.i = element;
-        this.i.size = {
-            width: this.i.offsetWidth,
-            height: this.i.offsetHeight
-        };
-        options = options || {};
+        this.element = element;
         this.options = {
-            particleColor: options.particleColor !== undefined ? options.particleColor : '#fff',
-            background: options.background !== undefined ? options.background : '#1a252f',
+            particleColor: options.particleColor || '#fff',
+            background: options.background || '#1a252f',
             interactive: options.interactive !== undefined ? options.interactive : true,
             velocity: this.setVelocity(options.speed),
-            density: this.j(options.density)
+            density: this.setDensity(options.density)
         };
         this.init();
     }
 
     ParticleNetwork.prototype.init = function() {
-        this.k = document.createElement('div');
-        this.i.appendChild(this.k);
-        this.l(this.k, {
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0
-        });
-
-        if (/^#[0-9A-F]{6}$/i.test(this.options.background)) {
-            this.l(this.k, {
-                background: this.options.background
-            });
-        } else {
-            if (!/\.(gif|jpg|jpeg|tiff|png)$/i.test(this.options.background)) {
-                console.error('Please specify a valid background image or hexadecimal color');
-                return false;
-            }
-            this.l(this.k, {
-                background: 'url("' + this.options.background + '") no-repeat center',
-                'background-size': 'cover'
-            });
-        }
-
-        if (!/^#[0-9A-F]{6}$/i.test(this.options.particleColor)) {
-            console.error('Please specify a valid particleColor hexadecimal color');
-            return false;
-        }
-
-        this.canvas = document.createElement('canvas');
-        this.i.appendChild(this.canvas);
-        this.g = this.canvas.getContext('2d');
-        this.canvas.width = this.i.size.width;
-        this.canvas.height = this.i.size.height;
-
-        this.l(this.i, {
-            position: 'absolute'
-        });
-        this.l(this.canvas, {
-            position: 'absolute'
-        });
-
-        window.addEventListener('resize', function() {
-            if (this.i.offsetWidth === this.i.size.width && this.i.offsetHeight === this.i.size.height) {
-                return false;
-            }
-            this.canvas.width = this.i.size.width = this.i.offsetWidth;
-            this.canvas.height = this.i.size.height = this.i.offsetHeight;
-            clearTimeout(this.m);
-            this.m = setTimeout(function() {
-                this.o = [];
-                for (var a = 0; a < this.canvas.width * this.canvas.height / this.options.density; a++) {
-                    this.o.push(new Particle(this));
-                }
-                if (this.options.interactive) {
-                    this.o.push(this.p);
-                }
-                requestAnimationFrame(this.update.bind(this));
-            }.bind(this), 500);
-        }.bind(this));
-
-        this.o = [];
-        for (var a = 0; a < this.canvas.width * this.canvas.height / this.options.density; a++) {
-            this.o.push(new Particle(this));
-        }
-
-        if (this.options.interactive) {
-            this.p = new Particle(this);
-            this.p.velocity = {
-                x: 0,
-                y: 0
-            };
-            this.o.push(this.p);
-
-            this.canvas.addEventListener('mousemove', function(event) {
-                this.p.x = event.clientX - this.canvas.offsetLeft;
-                this.p.y = event.clientY - this.canvas.offsetTop;
-            }.bind(this));
-
-            this.canvas.addEventListener('mouseup', function() {
-                this.p.velocity = {
-                    x: (Math.random() - 0.5) * this.options.velocity,
-                    y: (Math.random() - 0.5) * this.options.velocity
-                };
-                this.p = new Particle(this);
-                this.p.velocity = {
-                    x: 0,
-                    y: 0
-                };
-                this.o.push(this.p);
-            }.bind(this));
-        }
-
-        requestAnimationFrame(this.update.bind(this));
+        this.createCanvas();
+        this.createParticles();
+        this.setupEventListeners();
+        this.animate();
     };
 
-    ParticleNetwork.prototype.update = function() {
-        this.g.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.g.globalAlpha = 1;
+    ParticleNetwork.prototype.createCanvas = function() {
+        this.canvas = document.createElement('canvas');
+        this.element.appendChild(this.canvas);
+        this.context = this.canvas.getContext('2d');
+        this.resizeCanvas();
+        this.element.style.position = 'relative';
+        this.canvas.style.position = 'absolute';
+        this.canvas.style.top = '0';
+        this.canvas.style.left = '0';
+        this.canvas.style.background = this.options.background;
+    };
 
-        for (var a = 0; a < this.o.length; a++) {
-            this.o[a].update();
-            this.o[a].h();
-            for (var b = this.o.length - 1; b > a; b--) {
-                var c = Math.sqrt(Math.pow(this.o[a].x - this.o[b].x, 2) + Math.pow(this.o[a].y - this.o[b].y, 2));
-                if (c > 120) {
-                    continue;
-                }
-                this.g.beginPath();
-                this.g.strokeStyle = this.options.particleColor;
-                this.g.globalAlpha = (120 - c) / 120;
-                this.g.lineWidth = 0.7;
-                this.g.moveTo(this.o[a].x, this.o[a].y);
-                this.g.lineTo(this.o[b].x, this.o[b].y);
-                this.g.stroke();
+    ParticleNetwork.prototype.resizeCanvas = function() {
+        this.canvas.width = this.element.offsetWidth;
+        this.canvas.height = this.element.offsetHeight;
+    };
+
+    ParticleNetwork.prototype.createParticles = function() {
+        this.particles = [];
+        var particleCount = Math.floor(this.canvas.width * this.canvas.height / this.options.density);
+        for (var i = 0; i < particleCount; i++) {
+            this.particles.push(new Particle({
+                canvas: this.canvas,
+                g: this.context,
+                options: this.options
+            }));
+        }
+        if (this.options.interactive) {
+            this.setupInteractiveParticle();
+        }
+    };
+
+    ParticleNetwork.prototype.setupInteractiveParticle = function() {
+        this.interactiveParticle = new Particle({
+            canvas: this.canvas,
+            g: this.context,
+            options: this.options
+        });
+        this.particles.push(this.interactiveParticle);
+        this.canvas.addEventListener('mousemove', this.updateInteractiveParticle.bind(this));
+        this.canvas.addEventListener('mouseup', this.releaseInteractiveParticle.bind(this));
+    };
+
+    ParticleNetwork.prototype.updateInteractiveParticle = function(event) {
+        this.interactiveParticle.x = event.clientX - this.canvas.offsetLeft;
+        this.interactiveParticle.y = event.clientY - this.canvas.offsetTop;
+    };
+
+    ParticleNetwork.prototype.releaseInteractiveParticle = function() {
+        this.interactiveParticle.velocity = {
+            x: (Math.random() - 0.5) * this.options.velocity,
+            y: (Math.random() - 0.5) * this.options.velocity
+        };
+        this.setupInteractiveParticle();
+    };
+
+    ParticleNetwork.prototype.animate = function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        for (var i = 0; i < this.particles.length; i++) {
+            var particle = this.particles[i];
+            particle.update();
+            particle.draw();
+            for (var j = i + 1; j < this.particles.length; j++) {
+                particle.drawConnection(this.particles[j]);
             }
         }
-
-        if (this.options.velocity !== 0) {
-            requestAnimationFrame(this.update.bind(this));
-        }
+        requestAnimationFrame(this.animate.bind(this));
     };
 
     ParticleNetwork.prototype.setVelocity = function(speed) {
@@ -184,31 +144,18 @@
         }
     };
 
-    ParticleNetwork.prototype.j = function(density) {
+    ParticleNetwork.prototype.setDensity = function(density) {
         if (density === 'high') {
             return 5000;
         } else if (density === 'low') {
             return 20000;
-        } else if (isNaN(parseInt(density, 10))) {
-            return 10000;
-        } else {
+        } else if (!isNaN(parseInt(density, 10))) {
             return parseInt(density, 10);
+        } else {
+            return 10000;
         }
     };
 
-    ParticleNetwork.prototype.l = function(element, styles) {
-        for (var property in styles) {
-            element.style[property] = styles[property];
-        }
-    };
+    global.ParticleNetwork = ParticleNetwork;
 
-    if (typeof define === 'function' && define.amd) {
-        define(['exports'], function(exports) {
-            global.ParticleNetwork = ParticleNetwork;
-        });
-    } else if (typeof module !== 'undefined' && module.exports) {
-        module.exports = ParticleNetwork;
-    } else {
-        global.ParticleNetwork = ParticleNetwork;
-    }
 })(typeof self !== 'undefined' ? self : typeof global !== 'undefined' ? global : this);
